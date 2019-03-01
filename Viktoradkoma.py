34552da5f58e42caf6760994315f5efe3b9cafdb
hrolfur97 <47600709+hrolfur97@users.noreply.github.com>
2019-03-01 16:30:38 +0000

    Brakandi fersku Hrollakóði

--- Viktoradkoma.py
+++ Viktoradkoma.py
@@ -2,20 +2,27 @@ from random import shuffle
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
-
-
-def Kynning():
+def kynning():
     print()
     print("Viktor er dularfullar og hljóðlátur maður og hefur ýmis leyndarmál að geyma. Það fer ekki mikið fyrir honum og vita fáir að það eina sem Viktor þráir í raun er ást, umhyggja og smá athygli.")
     print()
-    print("Verkefni þitt er að svara 8 laufléttum spurningum um Viktor og svara að lágmarki 5 af þeim rétt til þess að sýna Viktori að þú þekkir hann og kunnir að meta. Ef þetta tekst mun Viktor brjótast út úr skelinni, blómstra í námi sínu og útskrifast úr HÍ, ef ekki, mun hann falla úr skólanum!")
+    print("Verkefni þitt er að svara 10 laufléttum spurningum um Viktor og svara að lágmarki 5 af þeim rétt til þess að sýna Viktori að þú þekkir hann og kunnir að meta. Ef þetta tekst mun Viktor brjótast út úr skelinni, blómstra í námi sínu og útskrifast úr HÍ, ef ekki, mun hann falla úr skólanum!")
     print()
 
+def spurningar():
 
-
-def spurningar(spurningar):
+    spurningar = [("Fílar Viktor hávaða? (Já/Nei):", "Nei"),
+    ("Fílar Viktor að tala? (Já/Nei):", "Nei"),
+    ("Elskar Viktor þögnina? (Já/Nei):", "Já"),
+    ("Hvað er uppáhaldsdrykkurinn hans Viktors? (Coke Zero/Peru Nocco/Hvítur GoGo/Pepsi Max):", "Pepsi Max"),
+    ("Hvernig bíl á Vikki D? (Trabant/Lada Sport/Audi/Passat):", "Audi"),
+    ("Hvað á Viktor margar klippingar eftir? (0/3 max/1/∞):", "3 max"),
+    ("Hvað er uppáhalds tækjavörumerkið hans Viktors? (Apple/Dell/HP/Denver):", "Apple"),
+    ("Hver er besti hópfélagi Viktors? (Hrólfur/Aron/Þorgeir/Víkingur):", "Þorgeir"),
+    ("Bjó Vikki D til iPad gryfjuna? (Já/Nei/Ekki Lexi):", "Já"),
+    ("Hvað er prófælmyndin hans Viktors gömul? (2 vikna/3 mánaða/6 ára/Á ekki facebook):", "6 ára")
+    ]
+    shuffle(spurningar)
 
     print("Spurningarnar:")
     print()
@@ -39,21 +46,9 @@ def spurningar(spurningar):
         print("Þú skeist á þig og varst með " + str(fjoldirangt) + " spurningar rangar af " + str(heildarspurn) + ", Viktor hefur því miður fallið úr skólanum.")
 
 def main():
-    Kynning()
-    spurningar = [("Fílar Viktor hávaða? (Já/Nei):", "Nei"),
-    ("Fílar Viktor að tala? (Já/Nei):", "Nei"),
-    ("Elskar Viktor þögnina? (Já/Nei):", "Já"),
-    ("Hvað er uppáhaldsdrykkurinn hans Viktors? (Coke Zero/Peru Nocco/Hvítur GoGo/Pepsi Max):", "Pepsi Max"),
-    ("Hvernig bíl á Vikki D? (Trabant/Lada Sport/Audi/Passat)", "Audi"),
-    ("Hvað á Viktor margar klippingar eftir? (0/3 max/1/∞):", "3 max"),
-    ("Hvað er uppáhalds tækjavörumerkið hans Viktors? (Apple/Dell/HP/Denver):", "Apple"),
-    ("Hver er besti hópfélagi Viktors? (Hrólfur/Aron/Þorgeir/Víkingur):", "Þorgeir")]
-    shuffle(spurningar)
-    spurningar(spurningar)
 
-
-
-
+    kynning()
+    spurningar()
 
 if __name__ == "__main__":
     main()
