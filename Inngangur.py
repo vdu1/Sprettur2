85b06693348e187a90eb2d3ca74887389d9b45b8
Víkingur Goði Sigurðarson <vgs3@hi.is>
2019-03-26 14:56:52 +0000

    a

--- Inngangur.py
+++ Inngangur.py
@@ -1,12 +1,19 @@
+import time
+import sys
 class Inngangur: #Klasinn
 
     def __init__(self): #Smiðurinn, notum engan "smið" eins og er, eða hvað?
         pass
 
     def inngangur(self): #Fallið sem byrjar leikinn
-        #time.sleep(1)
-        print("\nAllir nemendur við Háskóla Íslands glíma við sín eigin vandamál og þurfa þeir að leysa þau ef"
-        " þeir ætla að útskrifast með sæmd úr\nháskólanum. Þitt verkefni er að komast í gegnum þrautir leiksins og ná að brautskrá sem flesta nemendur.")
+        inngangu = "\nAllir nemendur við Háskóla Íslands glíma við sín eigin vandamál og þurfa þeir að leysa þau ef þeir ætla að útskrifast með sæmd úr\nháskólanum. Þitt verkefni er að komast í gegnum þrautir leiksins og ná að brautskrá sem flesta nemendur."
+
+        for char in inngangu:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(.050)
+
+        print("\n")
         #time.sleep(5)
         print("\nHefjumst handa!")
         #time.sleep(1)
